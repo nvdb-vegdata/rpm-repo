9b7b0f155b517df294e2f6553ed17350d46ef9c0
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2018-03-21 17:10:13 +0100

    Servlet for downloading files

---
+++ plugins/reposervice/src/main/java/no/kantega/rpmrepo/RepoServlet.java
@@ -0,0 +1,103 @@
+package no.kantega.rpmrepo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+class RepoServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(RepoServlet.class);
+
+    private final String servletPath;
+    private final File repoDir;
+
+    public RepoServlet(String servletPath, File repoDir) {
+        this.servletPath = servletPath;
+        this.repoDir = repoDir;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fullPath = req.getRequestURI()
+                .replace("..", "");
+        String path = fullPath
+                .replace(servletPath, "");
+
+
+        log.info("Request: " + fullPath);
+        if(log.isDebugEnabled()) {
+            log.debug("Headers: " + getHeaders(req));
+        }
+
+        File repoPath = new File(repoDir, path);
+        if(!repoPath.exists()) {
+            resp.setStatus(404);
+        } else if(repoPath.isDirectory()) {
+            listContent(repoPath, fullPath, resp);
+        } else {
+            streamFile(repoPath, req.getServletContext(), resp);
+        }
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest req) {
+        Map<String, String> headers = new HashMap<>();
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headername = headerNames.nextElement();
+            headers.put(headername, req.getHeader(headername));
+        }
+        return headers;
+    }
+
+    private void streamFile(File repoFile, ServletContext servletContext, HttpServletResponse resp) throws IOException {
+        resp.setHeader("Content-Type", servletContext.getMimeType(repoFile.getName()));
+        resp.setHeader("Content-Length", String.valueOf(repoFile.length()));
+        resp.setHeader("Content-Disposition",
+                "attachment; filename=\"" + repoFile.getName() + "\"");
+
+        try(var fis = new FileInputStream(repoFile)) {
+            fis.transferTo(resp.getOutputStream());
+        }
+    }
+
+    private void listContent(File repoPath, String fullPath, HttpServletResponse resp) throws IOException {
+        resp.setHeader("Content-Type", "text/html; charset=UTF-8");
+        try (OutputStreamWriter out = new OutputStreamWriter(resp.getOutputStream())) {
+            out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">\n" +
+                    "<html>" +
+                    "<head><title>Index of " + fullPath + "</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "<h1>Index of " + fullPath + "</h1>\n" +
+                    "<pre>Name Last modified Size</pre><hr/>\n");
+            File[] files = repoPath.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    out.write("<a href=\"");
+                    out.write(file.getName());
+                    out.write("\">");
+                    out.write(file.getName());
+                    out.write("</a> ");
+                    out.write(new Date(file.lastModified()).toString());
+                    out.write("&nbsp");
+                    out.write(String.valueOf(file.length()));
+                    out.write(" bytes\n");
+                }
+            }
+            out.write("</pre></body></html>");
+        }
+    }
+}
--- plugins/reposervice/src/main/java/no/kantega/rpmrepo/ReposervicePlugin.java
+++ plugins/reposervice/src/main/java/no/kantega/rpmrepo/ReposervicePlugin.java
@@ -5,20 +5,29 @@ import org.kantega.reststop.api.Config;
 import org.kantega.reststop.api.Export;
 import org.kantega.reststop.api.Plugin;
 import org.kantega.reststop.jaxrsapi.ApplicationBuilder;
+import org.kantega.reststop.servlet.api.ServletBuilder;
 
+import javax.servlet.Filter;
 import javax.ws.rs.core.Application;
-import java.io.File;
+import java.io.*;
 
 @Plugin
 public class ReposervicePlugin {
 
     @Export
     final Application repoResource;
+    @Export
+    final Filter repoServlet;
 
-    public ReposervicePlugin(@Config String repoDirPath, ApplicationBuilder appBuilder) {
+    public ReposervicePlugin(@Config String repoDirPath,
+                             ApplicationBuilder appBuilder,
+                             ServletBuilder servletBuilder) {
+        File repoDir = new File(repoDirPath);
         repoResource = appBuilder.application()
-                .singleton(new RepoResource(new File(repoDirPath)))
+                .singleton(new RepoResource(repoDir))
                 .build();
+        String servletPath = "/rpm/noarch";
+        repoServlet = servletBuilder.servlet(new RepoServlet(servletPath, repoDir), servletPath + "/*");
     }
 
 }
